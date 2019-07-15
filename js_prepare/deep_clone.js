c4de6354bb15c5495433318fbb1da53cd55ebc36
archy <chinaarchy@gmail.com>
2019-07-15 11:37:19 +0800

    深拷贝

---
+++ js_prepare/deep_clone.js
@@ -0,0 +1,18 @@
+function deepClone(obj) {
+    // 判断当前参数为数组还是对象，并进行定义
+    let objArray = Array.isArray(obj)?[]:{};
+    if (obj && typeof obj === "object"){
+        for (key in obj) {
+            // 循环遍历内部的参数，判断是否为自己的参数
+            if (obj.hasOwnProperty(key)){
+                // 如果还是对象，递归调用
+                if (obj[key] && typeof obj[key] === "object"){
+                    objArray[key] = deepClone(obj[key])
+                } else {
+                    objArray[key] = obj[key]
+                }
+            }
+        }
+    }
+    return objArray
+}
